11fc521ccfd774dcebbbf714b0ae2a634cb33d2c
Michał Skwira <michalskwira@yahoo.com>
2018-12-06 19:09:52 +0100

    masakra

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,25 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-    entry: './src/index.js',
+const plugins = [
+    new HtmlWebpackPlugin({
+        template: 'src/index.html',
+        filename: 'index.html',
+        inject: 'body'
+    }),
+];
+
+module.exports = (env) => {
+    return {
+        mode: env || 'production',
+        entry: './src/index.js',
         output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'app.bundle.js'
-    },
-    module: {
-        rules: [
+            path: path.resolve(__dirname, 'build'),
+            filename: 'app.' + environment + '.bundle.js'
+        },
+
+        module: {
+            rules: [
             {
                 test: /\.js$/,
                 loader: "babel-loader"
@@ -25,5 +37,6 @@ module.exports = {
                 ]
             }
         ]
-    }
-};
+    },
+},
+},
